fb7f03839fb9d4bb45f262ca732bb66873b81ad2
naga272 <114398392+naga272@users.noreply.github.com>
2024-03-13 18:15:57 +0100

    Update main.c

--- Scooter/bin/Scooter/bin/main.c
+++ Scooter/bin/Scooter/bin/main.c
@@ -4,22 +4,17 @@
 
 #include "scooter.h"
 
-char* test(struct scooter* oggetto){
-
-	return "ho cambiato l'indirizzo del puntatore a funzione";
-}
-
-
 
 int main(int argc, char *argv[], char *envp[]){
 	setlocale(LC_CTYPE, " ");
-	struct scooter *p1 = Scooter(12.0, 13.0, 15, 18);
 
-	printf("capacità: %.2f\n", p1 -> capacita);
+	struct scooter *p1 = Scooter(12.0, 13.0, 15, 18); // creo un'istanza
+
 	p1 -> capacita = 18.0;
 
 	printf("capacità: %.2f\n", p1->capacita);
 
+	p1 -> __del__();		// libero la memoria allocata dinamicamente
 	return EXIT_SUCCESS;
 }
 
